ce27a03ec608dab9fef51f34a2e3d0ea9a861d06
Cyberarm <matthewlikesrobots@gmail.com>
2019-04-03 14:37:59 -0500

    Added mouse look, player clipping through walls is now an edge case.

--- lib/player.rb
+++ lib/player.rb
@@ -23,6 +23,10 @@ class Player
 
     @field_of_view = 45.0
     @view_distance = 50.0
+    @min_wall_distance = 0.25
+
+    @mouse_pos = @window.mouse_x
+    @mouse_sensitivity = 0.1
 
     @sky = Gosu::Color::BLACK
     @gl_sky = GLColor.new(@sky.red / 255.0, @sky.green / 255.0, @sky.blue / 255.0, @sky.alpha / 255.0)
@@ -55,6 +59,7 @@ class Player
     moved = move(normalized)
 
     bob_head if moved && @new_position.magnitude.abs > 0.001
+    mouse_look
     @new_position = Vector.new
   end
 
@@ -98,8 +103,8 @@ class Player
 
   def move(normalized)
     moved = true
-    x_tile = @map.tiles.dig((@position.x + normalized.x * speed).to_i, @position.z.to_i)
-    z_tile = @map.tiles.dig(@position.x.to_i, (@position.z + normalized.z * speed).to_i)
+    x_tile = @map.tiles.dig((@position.x + normalized.x * speed + (normalized.x * @min_wall_distance)).to_i, @position.z.to_i)
+    z_tile = @map.tiles.dig(@position.x.to_i, (@position.z + normalized.z * speed + (normalized.z * @min_wall_distance)).to_i)
 
     nx = Vector.new(normalized.x)
     nz = Vector.new(0, 0, normalized.z)
@@ -120,5 +125,13 @@ class Player
   def bob_head
     @head_bob_position += @head_bob_speed
     @position.y = (Math.sin(@head_bob_position) * @head_bob_factor) + @head_height
+  end
+
+  def mouse_look
+    x = @window.mouse_x
+    @orientation.y -= (@mouse_pos - x) * @mouse_sensitivity
+
+    @window.mouse_x = @window.width/2 if x < 1 || x > @window.width - 1
+    @mouse_pos = @window.mouse_x
   end
 end
