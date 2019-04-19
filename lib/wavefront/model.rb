9fbbcc1b17db5b204f1494f141a07354b47ea728
Cyberarm <matthewlikesrobots@gmail.com>
2019-04-18 19:42:49 -0500

    Recover from missing models with default model

--- lib/wavefront/model.rb
+++ lib/wavefront/model.rb
@@ -1,8 +1,13 @@
 class Wavefront
   class Model
-    def initialize(name = "default")
+    def initialize(name = :default)
       @name = name
       @objects = []
+
+      unless File.exist?("#{GAME_ROOT_PATH}/assets/models/#{name}/#{name}.obj")
+        warn "Model '#{name.inspect}' not found, using fallback."
+        name = :default
+      end
 
       @parser = Parser.new("#{GAME_ROOT_PATH}/assets/models/#{name}/#{name}.obj")
     end
